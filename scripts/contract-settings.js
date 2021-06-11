c39807cb44c4e05a2d28010c6c0a16d978096eb6
Edwin Fajardo <edwin@m1ghty.io>
2021-06-11 12:03:23 -0500

    removed comments

--- scripts/contract-settings.js
+++ scripts/contract-settings.js
@@ -7,7 +7,6 @@ const { settings } = contractNames
 async function setParamsValue (test = false) {
   if (test) params = testparams
   const keys = Object.keys(params)
-  // console.log(params)
 
   for (const key of keys) {
     await transact({
